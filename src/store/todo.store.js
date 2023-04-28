4e2d001f83569852a353b8da3d0a5af20304353b
Hugo <hugo@boahstudio.com>
2023-04-28 20:36:12 -0300

    html todo initial render

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -11,10 +11,11 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra del campo'),
     ],
     filter: Filters.All,
 }
-
 
 const initStore = () => {
     console.log(state);
@@ -25,12 +26,29 @@ const loadStore = () => {
     throw new Error('Not Implemented');
 }
 
+const getTodos = ( filter = Filters.All ) => {
+    switch ( filter) {
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+        
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+    
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+    }
+}
+
 /**
  * 
  * @param {String} description 
  */
 const addTodo = ( description ) => {
-    throw new Error('Not Implemented');
+    if (!description ) throw new Error('Description is required');
+    state.todos.push( new Todo(description) );
 }
 
 /**
@@ -38,7 +56,12 @@ const addTodo = ( description ) => {
  * @param {String} todoId 
  */
 const toggleTodo = ( todoId ) => {
-    throw new Error('Not Implemented');
+    state.todos = state.todos.map( todo => {
+        if( todo.id === todoId ) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    }); 
 }
 
 /**
@@ -46,15 +69,29 @@ const toggleTodo = ( todoId ) => {
  * @param {String} todoId 
  */
 const deleteTodo = ( todoId ) => {
-    throw new Error('Not Implemented');
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
 }
 
 const deleteCompleted = () => {
-    throw new Error('Not Implemented');
+    state.todos = state.todos.filter( todo => todo.done );
 }
 
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
 
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
-    loadStore
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
